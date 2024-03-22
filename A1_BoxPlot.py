bceac06f23f95072288182c7cbada870529f1e22
Furkan <furkanmar@outlook.com>
2024-03-22 23:39:20 +0300

    Box plot

---
+++ A1_BoxPlot.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df=pd.read_csv("../../../PycharmProjects/pythonProject2/GameSales.csv")
+#Box plot task
+def boxplot(df):
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20, 5))
+    # na sales
+
+    axes[0, 0].boxplot(df["NA_Sales"])
+    axes[0, 0].set_title("North America Sales Box Plot")
+    # eu sales
+    axes[0, 1].boxplot(df["EU_Sales"])
+    axes[0, 1].set_title("Europe Sales Box Plot")
+    # japan sales
+    axes[1, 0].boxplot(df["JP_Sales"])
+    axes[1, 0].set_title("Japan Sales Box Plot")
+    # other sales
+    axes[1, 1].boxplot(df["Other_Sales"])
+    axes[1, 1].set_title("Other Sales Box Plot")
+    # show the grap
+    plt.tight_layout()
+    plt.show()
+
+boxplot(df)
+
